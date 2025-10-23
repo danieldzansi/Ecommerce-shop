b78489fb0f3d9b9617d4766302d118816f5fc992
danieldzansi <danieldzansi96@gmail.com>
2025-10-23 19:53:57 +0000

    Fix file name case for Home.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
 import Collection from './pages/collection'
 import About from './pages/about'
 import Contact from './pages/contact'
@@ -15,7 +15,7 @@ const App = () => {
     <div className='px-4 sm:px-[7vw] lg:px-[9vw]'>
       <Navbar/>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Home/>}/>
         <Route path='/collection' element={<Collection />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
